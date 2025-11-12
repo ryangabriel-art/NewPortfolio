75abc56892714b24ae46d8d17dd5aeac9272bb72
ryangabriel-art <ryan.gabriel@nave.org.br>
2025-11-12 08:20:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function insta(){
     window.location.href = "https://www.instagram.com/ryan_gabriel_figueiroa_flor/";
 }
+
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/ryan-gabriel-98490b398/";
+}
+
+
+
